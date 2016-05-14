d7d55ce58c79ebac6107e7a25ce64996f250603f
gautamh <gautamhathi@live.com>
2016-05-14 10:07:01 +0500

    fixed async tests

--- test/index_test.js
+++ test/index_test.js
@@ -5,11 +5,28 @@ describe("webhook server", function() {
   describe("homepage", function() {
     var url = "http://localhost:8000/";
 
-    it("responds with hello world message", function() {
+    it("responds with hello world message", function(done) {
       request(url, function(error, response, body) {
-        expect(response).to.equal("Hello World, this is chatbot");
+        console.log("Response body: " + response.body);
+        expect(response.body).to.equal("Hello World, this is chatbot");
         done();
       });
+    });
+  });
+
+  describe("GET verification", function () {
+    var url = "http://localhost:8000/webhook";
+
+    it("sends back the hub.challenge parameter", function(done) {
+      var propertiesObject = { 'hub.verify_token': 'test',
+                               'hub.challenge': '12345' };
+
+      request({url:url, qs:propertiesObject}, 
+              function(error, response, body) {
+                console.log("Response body: " + response.body);
+                expect(response.body).to.equal("12345");
+                done();
+              });
     });
   });
 });
